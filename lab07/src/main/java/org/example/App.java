ba182d505a96d2da8bdb87fd7b4f2b6c528eb32c
@sev4kz <vsevolod_trusov_00@mail.ru>
2022-11-07 17:22:33 +0300

    text

--- lab07/src/main/java/org/example/App.java
+++ lab07/src/main/java/org/example/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Calculator calculator = new Calculator();
+        System.out.println( "Hello CI!" );
+        System.out.println( "2 + 3 = " + calculator.getAmount(2, 3) );
+        System.out.println( "2 - 3 = " + calculator.getDifference(2, 3) );
+        System.out.println( "2 * 3 = " + calculator.getProduct(2, 3) );
+        System.out.println( "2 / 3 = " + calculator.getQuotient(2, 3) );
+        System.out.println( "2 % 3 = " + calculator.getRemainder(2, 3) );
     }
 }
